37e76bb435d640bde51c0f074219fdb180d2face
el-damiano <el9damiano@gmail.com>
2024-05-07 18:06:00 +0200

    feat: add count_letters

--- main.py
+++ main.py
@@ -3,10 +3,28 @@
 def count_words(input: str) -> int:
     return len(input.split())
 
+def count_letters(input: str) -> dict[str, int]:
+    letter_to_count = {}
+    lowered_string = input.lower()
+
+    for s in lowered_string:
+        if s not in letter_to_count:
+            letter_to_count[s] = 0
+
+        letter_to_count[s] += 1
+    
+    return letter_to_count
+
 def main():
     with open('books/frankenstein.txt') as f:
+        print(f"--- Begin report of {f.name} ---")
+
         file_contents = f.read()
-        print(f"total word count: {count_words(file_contents)}")
+        print(f"{count_words(file_contents)} words found in the document")
+        print(f"total letter count: {count_letters(file_contents)}")
+
+        print('--- End report ---')
+
 
 if __name__ == "__main__":
     main()
